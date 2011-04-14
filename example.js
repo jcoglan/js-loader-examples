4e544ae96ee550750072a693383fdf16b7cea70c
James Coglan <jcoglan@gmail.com>
2011-04-14 11:18:31 +0100

    Tidy up packages defs a little.

--- example.js
+++ example.js
@@ -3,63 +3,39 @@ var setupPackages = function() {
       var PATH = './lib/';
       var module = function(name) { return file(PATH + name + '.js') };
       
-      module('core')          .provides('JS.Module',
-                                        'JS.Class',
-                                        'JS.Method',
-                                        'JS.Kernel',
-                                        'JS.Singleton',
-                                        'JS.Interface');
+      module('core')        .provides('JS.Module', 'JS.Class', 'JS.Singleton');
       
-      module('test')          .provides('JS.Test')
-                              .requires('JS.Module',
-                                        'JS.Class',
-                                        'JS.Console',
-                                        'JS.DOM',
-                                        'JS.Enumerable',
-                                        'JS.SortedSet',
-                                        'JS.Comparable',
-                                        'JS.StackTrace')
-                              .styling(PATH + 'assets/testui.css');
+      module('test')        .provides('JS.Test')
+                            .requires('JS.Module', 'JS.Class', 'JS.Console', 'JS.DOM',
+                                      'JS.Enumerable', 'JS.SortedSet', 'JS.Comparable',
+                                      'JS.StackTrace')
+                            .styling(PATH + 'assets/testui.css');
       
-      module('dom')           .provides('JS.DOM',
-                                        'JS.DOM.Builder')
-                              .requires('JS.Class');
+      module('dom')         .provides('JS.DOM')
+                            .requires('JS.Class');
       
+      module('console')     .provides('JS.Console')
+                            .requires('JS.Module', 'JS.Enumerable');
 
-      module('console')       .provides('JS.Console')
-                              .requires('JS.Module',
-                                        'JS.Enumerable');
-
-      module('comparable')    .provides('JS.Comparable')
-                              .requires('JS.Module');
+      module('comparable')  .provides('JS.Comparable')
+                            .requires('JS.Module');
       
-      module('enumerable')    .provides('JS.Enumerable')
-                              .requires('JS.Module',
-                                        'JS.Class');
+      module('enumerable')  .provides('JS.Enumerable')
+                            .requires('JS.Module', 'JS.Class');
       
-      module('observable')    .provides('JS.Observable')
-                              .requires('JS.Module');
+      module('hash')        .provides('JS.Hash', 'JS.OrderedHash')
+                            .requires('JS.Class', 'JS.Enumerable', 'JS.Comparable');
       
-      module('hash')          .provides('JS.Hash',
-                                        'JS.OrderedHash')
-                              .requires('JS.Class',
-                                        'JS.Enumerable',
-                                        'JS.Comparable');
+      module('set')         .provides('JS.Set', 'JS.HashSet', 'JS.OrderedSet', 'JS.SortedSet')
+                            .requires('JS.Class', 'JS.Enumerable')
+                            .uses(    'JS.Hash');
       
-      module('set')           .provides('JS.Set',
-                                        'JS.HashSet',
-                                        'JS.OrderedSet',
-                                        'JS.SortedSet')
-                              .requires('JS.Class',
-                                        'JS.Enumerable')
-                              .uses(    'JS.Hash');
+      module('observable')  .provides('JS.Observable')
+                            .requires('JS.Module');
       
-      module('stack_trace')   .provides('JS.StackTrace')
-                              .requires('JS.Module',
-                                        'JS.Singleton',
-                                        'JS.Observable',
-                                        'JS.Enumerable',
-                                        'JS.Console');
+      module('stack_trace') .provides('JS.StackTrace')
+                            .requires('JS.Module', 'JS.Singleton', 'JS.Observable',
+                                      'JS.Enumerable', 'JS.Console');
   }});
 };
 
